77ec2f5db0782e37d1ad65a483887fee42f6dcf7
hiflyer <per.malmen@gmail.com>
2015-02-25 21:42:00 +0100

    The login handler now returns json containing the session key on successful login

--- src/main/java/se/fearless/spaceship/edge/LoginHandler.java
+++ src/main/java/se/fearless/spaceship/edge/LoginHandler.java
@@ -4,10 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.reactivex.netty.protocol.http.server.HttpServerRequest;
 import io.reactivex.netty.protocol.http.server.HttpServerResponse;
 import rx.Observable;
-import rx.functions.Action0;
-import rx.functions.Func1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,10 +15,15 @@ public class LoginHandler {
 	public Observable<Void> handle(HttpServerRequest<ByteBuf> request, final HttpServerResponse<ByteBuf> response) {
 		String userName = request.getQueryParameters().get("userName").get(0);
 		if (users.contains(userName)) {
-			response.writeString("Correct user\n");
+			writeSessionKey(response, userName);
 		} else {
-			response.writeString("Unknown user\n");
+			response.writeString("Unknown user");
 		}
+		response.writeString("\n");
 		return response.close();
+	}
+
+	private void writeSessionKey(HttpServerResponse<ByteBuf> response, String userName) {
+		response.writeString("{sessionKey:'"+ userName + "'}");
 	}
 }
