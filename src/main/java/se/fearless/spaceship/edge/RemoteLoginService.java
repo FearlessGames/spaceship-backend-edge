c70cf61e989fd3c35d9c866ecd6daaf01a3e59d5
hiflyer <per.malmen@gmail.com>
2015-02-25 22:49:25 +0100

    RemoteLoginService now talks to the authservice to authenticate

--- src/main/java/se/fearless/spaceship/edge/RemoteLoginService.java
+++ src/main/java/se/fearless/spaceship/edge/RemoteLoginService.java
@@ -1,24 +1,38 @@
 package se.fearless.spaceship.edge;
 
+import io.netty.buffer.ByteBuf;
+import io.reactivex.netty.RxNetty;
+import io.reactivex.netty.protocol.http.client.HttpClientResponse;
 import rx.Observable;
+import rx.functions.Func1;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
 public class RemoteLoginService {
 
-	private final List<String> users = Arrays.asList("hiflyer", "demazia");
+	private static final String LOGIN_SERVER = "http://localhost:9999";
 
 	Observable<LoginResult> login(String userName) {
-		LoginResult loginResult = getLoginResult(userName);
-		return Observable.just(loginResult);
+		Observable<HttpClientResponse<ByteBuf>> httpGet = RxNetty.createHttpGet(LOGIN_SERVER + "/" + userName);
+		return httpGet.flatMap(new Func1<HttpClientResponse<ByteBuf>, Observable<LoginResult>>() {
+			@Override
+			public Observable<LoginResult> call(HttpClientResponse<ByteBuf> byteBufHttpClientResponse) {
+				Observable<ByteBuf> content = byteBufHttpClientResponse.getContent();
+
+				return content.map(byteBuf -> {
+					if (byteBuf.toString(Charset.defaultCharset()).equals("SUCCESS")) {
+						return LoginResult.success();
+					} else {
+						return LoginResult.failed();
+					}
+
+				});
+
+			}
+		});
+
 	}
 
-	private LoginResult getLoginResult(String userName) {
-		if (users.contains(userName)) {
-			return LoginResult.success();
-		} else {
-			return LoginResult.failed();
-		}
-	}
 }
